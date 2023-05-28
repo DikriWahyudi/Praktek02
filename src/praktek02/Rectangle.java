bb4d46434f471a71f0f9ef9249671496e73dbfe3
DikriWahyudi <wahyudidikri135@gmail.com>
2023-05-28 09:04:34 +0800

    Membuat Method dengan Nilai Balik

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,4 +1,21 @@
 package praktek02;
 public class Rectangle {
+    double Panjang;
+    double Lebar;
 
+    
+    void cetakInfo(){
+        System.out.println("=========================");
+        System.out.println("Panjang :" + Panjang);
+        System.out.println("Lebar :" + Lebar);
+        System.out.println("=========================");
+    }
+    
+    double hitungLuas(){
+        double Luas;
+        Luas = Panjang * Lebar;
+        
+        return Luas;
+    }
+   
 }
